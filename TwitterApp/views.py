5e8bfc69df9813e1242fe0e55ef17ae4a19f1946
anil-techasoft <anil.kumar@techasoft.com>
2023-04-20 17:08:36 +0530

    Tweet API view created

--- TwitterApp/views.py
+++ TwitterApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializers import *
+from rest_framework.viewsets import ModelViewSet
+
 
 # Create your views here.
+class TweetViewSet(ModelViewSet):
+    queryset = Tweet.objects.all()
+    serializer_class = TweetSerializer
