0204053bdd101e414f8b89ce036541f4c752b3cb
anil-techasoft <anil.kumar@techasoft.com>
2023-04-20 14:10:44 +0530

    models created for twitter clone application

--- TwitterApp/models.py
+++ TwitterApp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Follower(models.Model):
+    follower_id = models.IntegerField(primary_key=True)
+    follower_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    following_user_id = models.ManyToManyField(User)
+
+
+class Tweet(models.Model):
+    tweet_id = models.IntegerField(primary_key=True)
+    tweet = models.CharField(max_length=200)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+
+
+class Reply(models.Model):
+    reply_id = models.IntegerField(primary_key=True)
+    tweet_id = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    reply = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+
+
+class Like(models.Model):
+    like_id = models.IntegerField(primary_key=True)
+    tweet_id = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+
