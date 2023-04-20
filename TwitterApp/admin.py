74cf54f1ac6fabc1a49c082039b7ec38ab7061dd
anil-techasoft <anil.kumar@techasoft.com>
2023-04-20 17:08:04 +0530

    TwitterApp models register in admin panel

--- TwitterApp/admin.py
+++ TwitterApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Tweet)
+admin.site.register(Follower)
+admin.site.register(Reply)
+admin.site.register(Like)
